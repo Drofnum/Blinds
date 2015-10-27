ea459a6555febde3f2cf65580a805af288a4b3e7
Drofnum <echaosaj@gmail.com>
2015-10-27 14:18:26 -0600

    Update blinds.ino

--- blinds.ino
+++ blinds.ino
@@ -55,7 +55,7 @@ void setup(void) {
     AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
-  myMotor->setSpeed(50);  // 10 rpm 
+  myMotor->setSpeed(10);  // 10 rpm 
 }
 
 void loop(void) {
